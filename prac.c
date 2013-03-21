3b6ec3bd39499d70f8ce62c1b1f37383d3b161de
frolov2 <frolov2@pclinux223-2.wud.lan>
2013-03-21 11:55:06 +0300

    create execle 21.03.2013

--- prac.c
+++ prac.c
@@ -2,17 +2,21 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void main()
+void main(int argv, char *argc[], char *envp[])
 {
-  
   int pid = fork();
-  
-  FILE * f1;
- 
-  f1 = fopen("temp.txt", "wb");
-  fprintf(f1, "%d", pid);
-  fclose(f1);
-  
-  pid = fork();
-  printf("%d", pid);
+
+  if(pid < 0)
+    printf("error\n");
+  else if (pid == 0)
+    {
+      FILE * f1 = fopen("first-one.txt", "wt");
+      fprintf(f1, "%d", pid);
+      fclose(f1);
+      execle("/usr/bin/env", "/usr/bin/env", NULL, envp);
+    }
+  else
+    {
+      printf("%d", pid);
+    }
 }
